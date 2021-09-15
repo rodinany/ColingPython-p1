1669ea99f1ed0464902412d02e3f53160dd15511
Anna Rodina <rodinany@google.com>
2021-09-15 13:18:30 +0300

    task D completed

--- HW1/D/D.py
+++ HW1/D/D.py
@@ -1,3 +1,9 @@
 def solution(total):
-    ### YOUR CODE HERE ###
-    return
+    hours = total // 60
+    minutes = total - hours * 60
+    while hours > 23:
+        hours -= 24
+    if minutes == 0:
+        minutes = f'0{minutes}'
+    total = f'{hours} {minutes}'
+    return total
