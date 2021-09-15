7b7adf55318d127d0fcb1e444c01296489111ec0
Anna Rodina <rodinany@google.com>
2021-09-15 13:53:11 +0300

    task E completed

--- HW1/E/E.py
+++ HW1/E/E.py
@@ -1,3 +1,7 @@
 def solution(x1, y1, x2, y2):
-    ### YOUR CODE ###
-    return
+    if (x2-x1==1 or x1==x2 or x1-x2==1) and (y1-y2==1 or y1==y2 or y2-y1==1):
+        return True
+    else:
+        return False
+
+    
