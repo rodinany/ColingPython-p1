be5b076c286869b7b4fce88b5cd9466e43fecb97
Анна Родина <you@example.com>
2021-09-23 00:10:27 +0300

    task A completed

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -1,3 +1,13 @@
 def solution(arr):
-    ### YOUR CODE ###
-    return
+    amount = []
+    n = 1
+    if arr:
+        amount.append(n)
+
+    for x in range(0, len(arr)-1):
+        if arr[x] == arr[x+1]:
+            n += 1
+        else:
+            amount.append(n)
+            n = 1
+    return max(amount)
