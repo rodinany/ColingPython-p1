ce4d421ce058b63176ad099ff4fde60180113bfd
Анна Родина <you@example.com>
2021-09-23 01:46:44 +0300

    task B completed

--- HW2/B/B.py
+++ HW2/B/B.py
@@ -1,3 +1,28 @@
 def solution(x):
-    ### YOUR CODE ###
-    return
+    l = len(x)
+    if 'h' in x:
+        h_amount = x.count('h')
+        n = 0
+        for i in range(l):
+            if x[i] == 'h':
+                n += 1
+                if n > 1 and n < h_amount:
+                    x = x[:i] + 'H' + x[i+1:]
+                    l = len(x)
+                else:
+                    continue
+    m = ''
+    for i in range(len(x)):
+        if i == 0 or i % 3 != 0:
+            m = m + x[i]
+    x = m
+    l = len(x)
+    while True:
+        if '1' in x:
+            for i in range(l):
+                if x[i] == '1':
+                    x = x[:i] + 'one' + x[i+1:]
+                    l = len(x)
+        else:
+            break
+    return x
