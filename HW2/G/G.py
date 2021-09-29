453724f73dddc3b947b2cdbd84f6734e07149ee6
Анна Родина <you@example.com>
2021-09-30 00:37:13 +0300

    task G completed

--- HW2/G/G.py
+++ HW2/G/G.py
@@ -1,3 +1,26 @@
 def solution(a, b):
-    ### YOUR CODE ###
-    return
+    c = []
+    d, e = tuple(a[:]), tuple(b[:])
+    while a and b:
+        if a[0] != b[0]:
+            if min(a[0],b[0]) == b[0]:
+                if b[0] not in d:
+                    c.append(b[0])
+                    del b[0]
+                else:
+                    del b[0]
+            else:
+                c.append(a[0])
+                del a[0]
+        else:
+            c.append(a[0])
+            del a[0]
+            del b[0]
+    x = max(a,b)
+    for i in x:
+        if x == b:
+            if i in e and i not in d:
+                c.append(i)
+        else:
+            c.append(i)
+    return c
