eea44cb6f4a9a9eedffb95e324b899f2e3a1ee9e
Анна Родина <you@example.com>
2021-09-30 00:34:56 +0300

    task d completed

--- HW2/D/D.py
+++ HW2/D/D.py
@@ -1,3 +1,12 @@
 def solution(n,k):
-    ### YOUR CODE HERE ###
-    return
+    soldiers = list(range(1, n+1))
+    l = 0
+    while len(soldiers) != 1:
+        for x in range(len(soldiers)):
+            l += 1
+            if l == k:
+                del soldiers[x]
+                soldiers = soldiers[x:] + soldiers[:x]
+                l = 0
+                break
+    return soldiers[0]
