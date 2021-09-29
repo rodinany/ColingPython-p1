a98332019d71a8bc7ded12f8938693b3772e0ab3
Анна Родина <you@example.com>
2021-09-30 00:34:04 +0300

    task C completed

--- HW2/C/C.py
+++ HW2/C/C.py
@@ -1,3 +1,7 @@
 def solution(arr):
-    ### YOUR CODE ###
-    return
+    new_arr = []
+    while arr:
+        new_arr += arr.pop(0)
+        arr = list(zip(*arr))
+        arr.reverse()
+    return new_arr
