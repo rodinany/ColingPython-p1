ccd7e676e29f79b6feb8d78f1414bc922117fb43
rodinany <90153707+rodinany@users.noreply.github.com>
2021-10-13 22:28:09 +0300

    Delete HW2 directory

--- HW2/C/C.py
+++
@@ -1,7 +0,0 @@
-def solution(arr):
-    new_arr = []
-    while arr:
-        new_arr += arr.pop(0)
-        arr = list(zip(*arr))
-        arr.reverse()
-    return new_arr
--- HW2/E/E.py
+++
@@ -1,5 +0,0 @@
-def solution(a, b):
-    if b == 0:
-        return a
-    else:
-        return solution(a+1, b-1)
