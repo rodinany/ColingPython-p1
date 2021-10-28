0247e19a8de4265d4de5a65a83f370daa5faa08a
rodina <rodinany@gmail.com>
2021-10-28 11:38:16 +0300

    __setitem__ added

--- HW3/A.py
+++ HW3/A.py
@@ -4,8 +4,16 @@ class OneIndexedList:
         self.items = items or []
 
     def __getitem__(self, idx):
-        return self.items[idx-1]
+         return self.items[idx-1]
+
+    def __setitem__(self, idx, value):
+        self.items[idx-1] = value
+        return value
 
 a = OneIndexedList([])
 a.items.append(0)
 print(a[1])
+
+b = OneIndexedList([1,2,3,4,5,6])
+b[4] = 100
+print(b[4])
