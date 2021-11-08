88c1618bce573998098a060e682f0b7e9e572a89
rodina <rodinany@gmail.com>
2021-11-09 00:38:28 +0300

    D modified

--- HW1/D/D.py
+++ HW1/D/D.py
@@ -1,8 +1,8 @@
 def solution(total):
     hours = total // 60
     minutes = total - hours * 60
-    while hours > 23:
-        hours -= 24
+    if hours > 23:
+        hours = hours % 24
     if minutes == 0:
         minutes = f'0{minutes}'
     total = f'{hours} {minutes}'
