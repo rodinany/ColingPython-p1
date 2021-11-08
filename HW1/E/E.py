149d01cee68580b2277e1fd6bd47c726df4d56be
rodina <rodinany@gmail.com>
2021-11-09 00:39:07 +0300

    E modified

--- HW1/E/E.py
+++ HW1/E/E.py
@@ -1,7 +1,6 @@
 def solution(x1, y1, x2, y2):
     if (x2-x1==1 or x1==x2 or x1-x2==1) and (y1-y2==1 or y1==y2 or y2-y1==1):
         return True
-    else:
-        return False
+    return False
 
     
