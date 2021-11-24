5fcc824baeaff808413802cf06157b5c7a0cd36d
rodina <rodinany@gmail.com>
2021-11-24 14:44:13 +0300

    C changed

---
+++ C.py
@@ -0,0 +1,35 @@
+import pymorphy2
+from itertools import product
+import random
+
+def generator(arr1, arr2):
+    random.shuffle(arr1)
+    random.shuffle(arr2)
+    for i in product(arr1, arr2, repeat=1):
+        n = morph.parse(i[0])[0]
+        k = morph.parse(i[1])[0]
+        if n.tag.gender == k.tag.gender and n.tag.case == k.tag.case == 'nomn':
+            yield i
+        else:
+            continue
+
+morph = pymorphy2.MorphAnalyzer()
+
+nouns = []
+adjectives = []
+
+with open('rus_shuffled.txt', 'r', encoding="utf-8") as f:
+    for word in f:
+        word = word.strip('\n')
+        p = morph.parse(word)[0]
+        if p.tag.POS == 'NOUN':
+            nouns.append(word)
+        elif p.tag.POS == 'ADJF':
+            adjectives.append(word)
+
+print(next(generator(nouns, adjectives)))
+print(next(generator(nouns, adjectives)))
+print(next(generator(nouns, adjectives)))
+print(next(generator(nouns, adjectives)))
+print(next(generator(nouns, adjectives)))
+print(next(generator(nouns, adjectives)))
